3e83b77f0ed83f14bd9c344418f2ee599001a6d7
Senki <senki.laboratory@gmail.com>
2022-10-13 23:44:50 +0900

    pythonで情報取得

---
+++ spotify-python/main.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import configparser
+
+config = configparser.ConfigParser()
+config.read('spotify-python/config.ini')
+
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+my_id = config.get('SpotifyAPI','client_id')
+my_secret = config.get('SpotifyAPI','client_secret')
+ccm = SpotifyClientCredentials(client_id = my_id, client_secret = my_secret)
+spotify = spotipy.Spotify(client_credentials_manager = ccm)
+results = spotify.artist_top_tracks(lz_uri)
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    print('audio    : ' + track['preview_url'])
+    print('cover art: ' + track['album']['images'][0]['url'])
+    print()
